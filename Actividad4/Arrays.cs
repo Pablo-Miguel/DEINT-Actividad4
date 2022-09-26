d4b914e9af28ec8b796c2ad7ad8ffc79e43f5b9b
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2022-09-26 11:03:40 +0200

    Agregar archivos de proyecto.

---
+++ Actividad4/Arrays.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+
+namespace Actividad4
+{
+    public class Arrays
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int minValorArray(int[] array)
+        {
+            if (array.Length != 0)
+            {
+                int min = array[0];
+
+                foreach (int i in array)
+                {
+                    if (i < min)
+                    {
+                        min = i;
+                    }
+                }
+
+                return min;
+            }
+            else {
+                return 0;
+            }
+            
+            throw new NotImplementedException();
+        }
+        public static int[] invertirArray(int[] array)
+        {
+            if (array.Length != 0)
+            {
+                int[] arrayInvertido = new int[array.Length];
+
+                for (int i = arrayInvertido.Length, j = 0; i > 0; i--, j++)
+                {
+                    arrayInvertido[j] = array[i];
+                }
+
+                return arrayInvertido;
+            }
+            else {
+                return array;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public static int comprobarValorEnArray(int[] array, int valor)
+        {
+
+            for (int i = 0; i < array.Length; i++) {
+                if (array[i] == valor) {
+                    return i;
+                }
+            }
+
+            return 0;
+
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        public static int calcularModa(int[] entrada)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+    }
+}
